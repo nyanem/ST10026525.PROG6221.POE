a40ffa61d462b0436e92a158bdf98320a82464b9
Khanyi Mabuza <131602333+nyanem@users.noreply.github.com>
2024-04-11 20:53:34 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10026525.POE.PART1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
