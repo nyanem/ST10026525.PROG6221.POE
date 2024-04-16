5e47bff5f3b6690d7e54a30bd09bcc7e60337c37
Khanyi Mabuza <131602333+nyanem@users.noreply.github.com>
2024-04-16 22:35:37 +0200

    Step 3: Swicth case menu as well as scaling factor

--- Program.cs
+++ Program.cs
@@ -17,10 +17,57 @@ namespace ST10026525.POE.PART1
         {
             //Object calls for recipe to be displayed 
             recipeClass recipe = RecipeBook.CreateNew();
-            if (recipe != null )
+            if (recipe != null)
             {
-                recipe.displayRecipe();
-            } else { Console.WriteLine("Failed to create Recipe.") ; }
+                //creating a menue where user can choose what they wish to do with their reciper
+                bool scaled = false;
+                while (true)
+                {
+                    Console.WriteLine("Recipe Options:");
+                    Console.WriteLine("1. Display Recipe");
+                    Console.WriteLine("2. Scale Recipe");
+                    Console.WriteLine("3. Exit");
+
+                    Console.WriteLine("Choose number: ");
+                    string choice = Console.ReadLine();
+
+                    switch (choice)
+                    {
+                        case "1":
+                            recipe.displayRecipe();
+                            break;
+                        case "2":
+                            if (!scaled)
+                            {
+                                Console.WriteLine("Would you like your recipe to be halfed(X0.5), doubled (x2) or tripled (x3): ");
+                                double factor;
+                                if (double.TryParse(Console.ReadLine(), out factor))
+                                {
+                                    recipe.scaleCalculator(factor);
+                                    Console.WriteLine("Here are your new measurements!");
+                                    scaled = true;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Error please try again");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Complete :)");
+                            }
+                            break;
+                        case "3":
+                            Console.WriteLine("Goodbye :)");
+                            return;
+                        default:
+                            Console.WriteLine("Invalid choice. Please eneter a number from 1 to 3");
+                            break;
+                    }
+                }
+
+            }
+            else { Console.WriteLine("Failed :("); }
            
             Console.ReadKey();
         }
