00a3a2ace604d6f07ea8f7eaf7cf7b0c485185f7
Khanyi Mabuza <131602333+nyanem@users.noreply.github.com>
2024-04-16 12:35:36 +0200

    Step 1: Complete

--- RecipeBook.cs
+++ RecipeBook.cs
@@ -29,6 +29,16 @@ namespace ST10026525.POE.PART1
 
                 ingredients[i] = new ingredientClass(ingredientName, quantity, unit);
             }
+            Console.WriteLine("How many steps does this recipe have? ");
+            int numSteps = int.Parse(Console.ReadLine());
+
+            string[] steps = new string[numSteps];
+            for (int i = 0; i < numSteps; i++) 
+            { 
+                Console.WriteLine("Enter step " + (i + 1) + ": ");
+                steps[i] = Console.ReadLine();
+            }
+            return new recipeClass(recipeName, ingredients, steps);
         }
     }
 }
--- recipeClass.cs
+++ recipeClass.cs
@@ -10,22 +10,25 @@ namespace ST10026525.POE.PART1
     {
         public string Name { get; set; }
         public ingredientClass[] Ingredients { get; set; }
-        public string []Steps {  get; set; }
+        public string[] Steps { get; set; }
 
-        public recipeClass( string name, ingredientClass[] ingredients, string[] steps) 
+        public recipeClass(string name, ingredientClass[] ingredients, string[] steps)
         {
             Name = name;
-            Ingredients = ingredients; 
+            Ingredients = ingredients;
             Steps = steps;
         }
-        public void displayRecipe() 
+        public void displayRecipe()
         {
             Console.WriteLine("Recipe: " + Name);
             Console.WriteLine("Ingredents:");
-            foreach (var ingredient in Ingredients) { Console.WriteLine("-" + ingredient.Quantity + "" + ingredient.Unit + "" + ingredient.Name);
-                 }
+            foreach (var ingredient in Ingredients)
+            {
+                Console.WriteLine("-" + ingredient.Quantity + "" + ingredient.Unit + "" + ingredient.Name);
+            }
             Console.WriteLine("Steps: ");
-            for (int i = 0; i < Steps.Length; i++) {  Console.WriteLine((i +1) + ": " + Steps[i]); }
-        
+            for (int i = 0; i < Steps.Length; i++) { Console.WriteLine((i + 1) + ": " + Steps[i]); }
+
+        }
     }
 }
